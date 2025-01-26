ad53278a27000da296480a5ba97040bb7eb0a333
Paula <paula.farid9@gmail.com>
2025-01-26 17:24:06 +0200

    fifth commit

--- src/test/java/TestBase/BaseClass.java
+++ src/test/java/TestBase/BaseClass.java
@@ -31,7 +31,6 @@ public class BaseClass {
                     break;
             }
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().window().maximize();
             driver.get("http://live.techpanda.org/");
         }
             homePage=new HomePage(driver);
